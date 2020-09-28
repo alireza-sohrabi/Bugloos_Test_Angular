b0b3057f3a2a00f7e2514c107e4896cb131ec883
Alireza Sohrabi <pdn.info.es@gmail.com>
2020-09-28 22:30:22 +0330

    alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,63 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
+  constructor() {
+    if (!localStorage['form-filed']) {
+      localStorage['form-filed'] = [
+        {
+          formFields: [
+            {
+              rowIndex: null,
+              colIndex: 3,
+              name: 'Name',
+              lName: 'نام',
+              fieldType: 1,
+              required: true,
+              id: 'a8fe92a3-13f2-46c8-a4db-1ea6f48cf588',
+            },
+            {
+              rowIndex: null,
+              colIndex: 2,
+              name: 'LastName',
+              lName: 'نام خانوادگی',
+              fieldType: 1,
+              required: true,
+              id: 'f3359b75-77e1-48d3-8d0c-ebc5c62fdf69',
+            },
+            {
+              rowIndex: 0,
+              colIndex: 1,
+              name: 'Name',
+              lName: 'نام',
+              fieldType: 1,
+              required: true,
+              id: '0dde151f-7734-460e-b607-67ecc9d133ec',
+            },
+            {
+              rowIndex: null,
+              colIndex: 1,
+              name: 'Name',
+              lName: 'نام خانوادگی',
+              fieldType: 1,
+              id: '2e9fa83c-ae0d-4185-ab12-a5a708b36f09',
+            },
+            {
+              rowIndex: 0,
+              colIndex: 0,
+              name: 'LastName',
+              lName: 'نام‌خانوادگی',
+              fieldType: 1,
+              required: true,
+              id: '4dcdf70d-b92b-499e-af6d-0107c40aa863',
+            },
+          ],
+          name: 'منابع انسانی',
+          width: 1200,
+          id: '439809ae-6fb9-4fac-adaf-effd80943bd0',
+          description: 'برای ایجاد کارمند جدید',
+        },
+      ];
+    }
+  }
   title = 'bugloos';
 }
--- src/app/dist/app.component.js
+++ src/app/dist/app.component.js
@@ -10,33 +10,63 @@ exports.AppComponent = void 0;
 var core_1 = require("@angular/core");
 var AppComponent = /** @class */ (function () {
     function AppComponent() {
-        this.columnsDef = [
-            { headerName: 'نام فرم', field: 'title' },
-            { headerName: 'توضیحات', field: 'description' },
-        ];
-        this.forms = [
-            {
-                title: 'form1',
-                value: 1,
-                description: 'as well as measure the accessibility level of any color combination.'
-            },
-            {
-                title: 'form2',
-                value: 2,
-                description: 'Shaw has a little JavaScript one-liner that is very clever'
-            },
-            {
-                title: 'form3',
-                value: 3,
-                description: 'Looking for a complete course on getting into web development'
-            },
-            {
-                title: 'form4',
-                value: 4,
-                description: 'JavaScript to Git and basic back-end development'
-            },
-        ];
         this.title = 'bugloos';
+        if (!localStorage['form-filed']) {
+            localStorage['form-filed'] = [
+                {
+                    formFields: [
+                        {
+                            rowIndex: null,
+                            colIndex: 3,
+                            name: 'Name',
+                            lName: 'نام',
+                            fieldType: 1,
+                            required: true,
+                            id: 'a8fe92a3-13f2-46c8-a4db-1ea6f48cf588'
+                        },
+                        {
+                            rowIndex: null,
+                            colIndex: 2,
+                            name: 'LastName',
+                            lName: 'نام خانوادگی',
+                            fieldType: 1,
+                            required: true,
+                            id: 'f3359b75-77e1-48d3-8d0c-ebc5c62fdf69'
+                        },
+                        {
+                            rowIndex: 0,
+                            colIndex: 1,
+                            name: 'Name',
+                            lName: 'نام',
+                            fieldType: 1,
+                            required: true,
+                            id: '0dde151f-7734-460e-b607-67ecc9d133ec'
+                        },
+                        {
+                            rowIndex: null,
+                            colIndex: 1,
+                            name: 'Name',
+                            lName: 'نام خانوادگی',
+                            fieldType: 1,
+                            id: '2e9fa83c-ae0d-4185-ab12-a5a708b36f09'
+                        },
+                        {
+                            rowIndex: 0,
+                            colIndex: 0,
+                            name: 'LastName',
+                            lName: 'نام‌خانوادگی',
+                            fieldType: 1,
+                            required: true,
+                            id: '4dcdf70d-b92b-499e-af6d-0107c40aa863'
+                        },
+                    ],
+                    name: 'منابع انسانی',
+                    width: 1200,
+                    id: '439809ae-6fb9-4fac-adaf-effd80943bd0',
+                    description: 'برای ایجاد کارمند جدید'
+                },
+            ];
+        }
     }
     AppComponent = __decorate([
         core_1.Component({
